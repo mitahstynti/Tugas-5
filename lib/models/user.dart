cf9aa31ac28450ef63d629ea304edfd76ba7376e
mitahstynti <2006013@itg.ac.id>
2023-03-28 15:45:57 +0700

    inititial commit

---
+++ lib/models/user.dart
@@ -0,0 +1,39 @@
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+  final String? profilePhoto;
+  final String? phoneNumber;
+
+  User({
+    required this.id,
+    required this.name,
+    required this.username,
+    required this.email,
+    this.profilePhoto,
+    this.phoneNumber,
+  });
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      id: map["id"],
+      name: map["name"],
+      username: map["username"],
+      email: map["email"],
+      profilePhoto: map["profile_photo"],
+      phoneNumber: map["phone_number"],
+    );
+  }
+
+  factory User.dummy() {
+    return User(
+      id: 001,
+      name: "Mita Hestiyanti",
+      username: "Mitahstynti ",
+      email: "2006013@itg.ac.id",
+      profilePhoto: "https://i.postimg.cc/J0BtDqFR/mita.jpg",
+      phoneNumber: "08888888888",
+    );
+  }
+}
---
+++ lib/modules/news_detail_screen/news_detail_screen.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+class NewsDetailScreen extends StatefulWidget {
+  const NewsDetailScreen({
+    super.key,
+    required this.id,
+  });
+  final String id;
+
+  @override
+  State<NewsDetailScreen> createState() => _NewsDetailScreenState();
+}
+
+class _NewsDetailScreenState extends State<NewsDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              "https://i.postimg.cc/cJYPRJBy/3627127841.jpg",
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  const SizedBox(height: 10),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Liburan bareng keluarga',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Buat yang dijalan harap hati hati. Buat yang dihati, kapan kita jalan jalan?',
+                    maxLines: 10,
+                    textAlign: TextAlign.justify,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NewsHotScreen extends StatefulWidget {
+  const NewsHotScreen({
+    super.key,
+    required this.newsDetailHot,
+  });
+  final String newsDetailHot;
+
+  @override
+  State<NewsHotScreen> createState() => _NewsHotScreenState();
+}
+
+class _NewsHotScreenState extends State<NewsHotScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(
+              "https://i.postimg.cc/pXwZDJGr/syg.jpg",
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  const SizedBox(height: 10),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Ye Liburan Yeeee',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Libur Telah Tiba Horrayyy.. Libur telah tiba Horayy.. Hatiku Gembiraa',
+                    maxLines: 10,
+                    textAlign: TextAlign.justify,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
